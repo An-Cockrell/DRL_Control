d5e5db4728548e0b24c2d21c8e7263b90e50e123
daleblarie <daleblarie@gmail.com>
2020-08-19 09:30:59 -0400

    updating docker script

--- dockerScript.sh
+++ dockerScript.sh
@@ -7,6 +7,7 @@ pip3 install gym
 pip3 install matplotlib
 pip3 install keras
 pip3 install tensorflow_probability
+pip3 install tensorflow_addons
 pip3 install sklearn
 g++ -fPIC -Wall -shared -o Simulation.so ./CPP/*.cpp -std=c++11 -O30
 python3 ./DDPG.py
