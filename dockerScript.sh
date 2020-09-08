be8393a572a3bfae24e5ef0745437e17b2909bdd
daleblarie <DaleBLarie@gmail.com>
2020-09-08 09:23:40 -0400

    attempt to add rendering to docker

--- dockerScript.sh
+++ dockerScript.sh
@@ -1,5 +1,21 @@
 # sudo docker run --gpus all -it -v "$PWD":/home/dale/DRL_Control --rm tensorflow/tensorflow:latest-gpu sh /home/dale/DRL_Control/dockerScript.sh
 
+FROM tensorflow/tensorflow:latest-gpu
+
+# Dependencies for glvnd and X11.
+RUN apt-get update \
+  && apt-get install -y -qq --no-install-recommends \
+    libxext6 \
+    libx11-6 \
+    libglvnd0 \
+    libgl1 \
+    libglx0 \
+    libegl1 \
+    freeglut3-dev \
+  && rm -rf /var/lib/apt/lists/*
+# Env vars for the nvidia-container-runtime.
+ENV NVIDIA_VISIBLE_DEVICES all
+ENV NVIDIA_DRIVER_CAPABILITIES graphics,utility,compute
 
 cd /home/dale/DRL_Control
 pip3 install --upgrade pip
